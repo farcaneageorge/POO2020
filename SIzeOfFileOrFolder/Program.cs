18edd6daf600568b8d0f4d8df44b49be10c80e38
farcaneageorge <55544091+farcaneageorge@users.noreply.github.com>
2020-05-10 17:55:36 +0300

    Add files via upload

---
+++ SIzeOfFileOrFolder/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIzeOfFileOrFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce the path:");
+
+            string path = Console.ReadLine();
+            SizeOfFileOrFolder f1 = new SizeOfFileOrFolder(path);
+
+            f1.TotalSize();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SIzeOfFileOrFolder/SizeOfFileOrFolder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+namespace SIzeOfFileOrFolder
+{
+    public class SizeOfFileOrFolder
+    {
+        string path;
+        private static long size;
+        public SizeOfFileOrFolder(string path)
+        {
+            size = 0;
+            this.path = path;
+
+        }
+        public static long FileSize(string path)
+        {
+            var FS = new FileInfo(path);
+            return FS.Length;
+        }
+        public long DirectorySize(string path)
+        {
+
+
+            string[] files = Directory.GetFiles(path);
+            foreach (string fileName in files)
+            {
+                size += FileSize(fileName);
+            }
+
+
+
+            string[] subdirectoryEntries = Directory.GetDirectories(path);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                size += DirectorySize(subdirectory);
+            }
+
+            return size;
+        }
+        public void TotalSize()
+        {
+            Console.WriteLine("Total size of File/Folder is: {0} bytes.", size);
+        }
+    }
+}
