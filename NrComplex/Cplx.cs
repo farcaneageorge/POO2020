a7e0d4298af01aef0588287f5d364690a9a1aa13
farcaneageorge <55544091+farcaneageorge@users.noreply.github.com>
2020-05-10 17:34:01 +0300

    Add files via upload

---
+++ NrComplex/Cplx.cs
@@ -0,0 +1,82 @@
+﻿namespace NrComplex
+{
+    public class Cplx
+    {
+        private int Re;
+        private int Im;
+
+        public Cplx()
+        {
+            this.Re = 0;
+            this.Im = 0;
+        }
+        public Cplx(int Re)
+        {
+            this.Re = Re;
+            this.Im = 0;
+        }
+        public Cplx(int Re, int Im)
+        {
+            this.Re = Re;
+            this.Im = Im;
+        }
+        public Cplx Sum(Cplx b)
+        {
+            Cplx result = new Cplx();
+
+            result.Re = Re + b.Re;
+            result.Im = Im + b.Im;
+
+            return result;
+        }
+        public Cplx Substract(Cplx b)
+        {
+            Cplx result = new Cplx();
+            result.Re = Re - b.Re;
+            result.Im = Im - b.Im;
+
+            return result;
+        }
+        public Cplx Multiply(Cplx b)
+        {
+            Cplx result = new Cplx();
+            result.Re = Re * b.Re - Im * b.Im;
+            result.Im = Re * b.Im + Im * b.Re;
+
+            return result;
+        }
+
+        public void TrigonometricForm()
+        {
+            double r = System.Math.Sqrt(Re * Re + Im * Im);
+            double teta = System.Math.Atan(Im / Re);
+            System.Console.WriteLine($" Forma trigonometrica : {r}(cos({teta})+isin({teta}))");
+        }
+        public void PowTrigonometricForm(int pow)
+        {
+            double r = System.Math.Sqrt(Re * Re + Im * Im);
+            double teta = System.Math.Atan(Im / Re);
+            System.Console.WriteLine($" Forma trigonometrica : {r * pow}(cos({teta * pow})+isin({teta * pow}))");
+        }
+        public Cplx Pow(int power)
+        {
+            Cplx result = new Cplx();
+
+            double r = System.Math.Sqrt(Re * Re + Im * Im);
+            double teta = System.Math.Atan(Im / Re);
+            result.Re = (int)(r * power * (System.Math.Cos(teta * power)));
+            result.Im = (int)(r * power * (System.Math.Sin(teta * power)));
+
+            return result;
+
+
+        }
+        public override string ToString()
+        {
+            if (Im < 0)
+                return string.Format($"{Re} - {System.Math.Abs(Im)}i");
+            else
+                return string.Format($"{Re} + {Im}i");
+        }
+    }
+}
---
+++ NrComplex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NrComplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cplx a = new Cplx(4, 7);
+            Cplx b = new Cplx(3, 5);
+
+            Cplx suma = a.Sum(b);
+            Console.WriteLine(suma);
+
+            Cplx diferenta = a.Substract(b);
+            Console.WriteLine(diferenta);
+            a.TrigonometricForm();
+            a.PowTrigonometricForm(5);
+            Cplx putere = a.Pow(2);
+            Console.WriteLine(putere);
+            Console.ReadKey();
+
+        }
+    }
+}
