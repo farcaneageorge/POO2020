7ab5f91c09f9d877f5fc7c50c3eb1126bcd67867
farcaneageorge <55544091+farcaneageorge@users.noreply.github.com>
2020-05-27 14:13:46 +0300

    Add files via upload

---
+++ Elevi/Elevi/Elev.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Elevi
+{
+    class Elev
+    {
+        public string firstName;
+        public string lastName;
+        private int numberOfGrades;
+        private string[] grades;
+        private double average;
+        public string Name => lastName + firstName;
+        public string[] Grades => grades;
+        public double Average => average;
+
+        public Elev()
+        {
+
+        }
+
+
+
+        public Elev(string lastName, string firstName, string numberOfGrades, string[] grades)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.numberOfGrades = int.Parse(numberOfGrades);
+            this.grades = grades;
+        }
+
+        public Elev(string lastName, string firstName, double average)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.average = average;
+        }
+
+        public double GetAverage(string[] grades)
+        {
+            double average = 0;
+            foreach (var grade in grades)
+            {
+                average += int.Parse(grade);
+            }
+            average /= grades.Length;
+
+            return average;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - average: {this.average}";
+        }
+    }
+}
---
+++ Elevi/Elevi/MyOrder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Elevi
+{
+    public class MyOrder : IComparer<Elev>
+    {
+        public int Compare(Elev a, Elev b)
+        {
+            int compareAverage = a.Average.CompareTo(b.Average);
+            if (compareAverage == 0)
+            {
+                int compareLastName = a.lastName.CompareTo(b.lastName);
+                if (compareLastName == 0)
+                {
+                    return a.firstName.CompareTo(b.firstName);
+                }
+                return compareLastName;
+            }
+            return compareAverage;
+        }
+
+    }
+}
