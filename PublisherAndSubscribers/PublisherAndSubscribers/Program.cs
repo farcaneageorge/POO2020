bfb58e5faac4f06a389eea5a497ed9507b86e383
farcaneageorge <55544091+farcaneageorge@users.noreply.github.com>
2020-06-09 11:07:28 +0300

    Add files via upload

---
+++ PublisherAndSubscribers/PublisherAndSubscribers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherAndSubscribers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher p = new Publisher();
+
+            SubscriberA a = new SubscriberA();
+            SubscriberB b = new SubscriberB();
+
+            p.eveniment += new iteratie(a.GetInteger);
+            p.eveniment += new iteratie(b.GetNeighbors);
+
+            p.DeclansareEveniment();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PublisherAndSubscribers/PublisherAndSubscribers/Publisher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherAndSubscribers
+{
+    public delegate void iteratie(int x);
+
+    public class Publisher
+    {
+        public event iteratie eveniment;
+
+        public void DeclansareEveniment()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (eveniment != null)
+                {
+                    eveniment(i);
+                }
+            }
+        }
+    }
+
+    public class SubscriberA
+    {
+        public void GetInteger(int a)
+        {
+            Console.WriteLine(a);
+        }
+    }
+
+    public class SubscriberB
+    {
+        public void GetNeighbors(int b)
+        {
+            Console.WriteLine($"Neighbors of {b} are: {b - 1}, {b + 1}");
+        }
+    }
+}
